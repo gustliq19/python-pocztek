e4375c80bb1fbb8dbfc56c6e7363beb81587d38a
Tomasz Morawski <morawski79@gmail.com>
2024-04-15 18:39:38 +0200

    fixed "zad 3" in "pakiety_moduly"

--- code/pakiety_moduly/zad_3.py
+++ code/pakiety_moduly/zad_3.py
@@ -1,7 +1,9 @@
+import calculations.loan
+
 initial_value = float(input("Jaka jest kwota początkowa? "))
 percentage = float(input("Jakie jest oprocentowanie? "))
 loan_years = int(input("Jaki jest czas lokaty (w latach)? "))
 
-loan_end_value = code.pakiety_moduly.calculations.loan.calculate_loan_value(initial_value, percentage, loan_years)
+loan_end_value = calculations.loan.calculate_loan_value(initial_value, percentage, loan_years)
 
 print(f"Wartość lokaty po {loan_years} latach będzie równa: {loan_end_value:.2f} zł")
