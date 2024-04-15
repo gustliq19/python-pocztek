3e2e454f76b16963fe3ee6d430b509c09b016078
Tomasz Morawski <morawski79@gmail.com>
2024-04-16 00:22:56 +0200

    Poprawione imoprty w zadaniu 4 z lekcji "Konstrukot i pola obiektu"

--- code/obiekty/konstruktory_zad_4.py
+++ code/obiekty/konstruktory_zad_4.py
@@ -1,12 +1,11 @@
-from store import Order
-from store.Order import print_orders
+from store.Order import Order, print_orders
 from store.Product import generate_products
 
 if __name__ == "__main__":
 
-    order_1 = Order.Order("Tomasz", "Morawski", generate_products(3))
-    order_2 = Order.Order("Jan", "Kowalski", generate_products(5))
-    order_3 = Order.Order("Piotr", "Górski", generate_products(6))
+    order_1 = Order("Tomasz", "Morawski", generate_products(3))
+    order_2 = Order("Jan", "Kowalski", generate_products(5))
+    order_3 = Order("Piotr", "Górski", generate_products(6))
     order_list = [order_1, order_2, order_3]
 
     print("Lista zamówień:\n\n")
