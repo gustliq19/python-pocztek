d21c3fa0bdb8a7e80ad3693f381c5278fbf9ec4d
Tomasz Morawski <morawski79@gmail.com>
2024-04-16 16:31:39 +0200

    Rozwiązanie zadania 2 z lekcji "Metoda"

--- code/obiekty/konstruktory_zad_4.py
+++ code/obiekty/konstruktory_zad_4.py
@@ -1,5 +1,7 @@
 from store.Order import Order
 from store.Product import generate_products
+from store.Apple import Apple
+from store.Potato import Potato
 
 if __name__ == "__main__":
 
@@ -11,3 +13,15 @@ if __name__ == "__main__":
     print("Lista zamówień:\n\n")
     for order in order_list:
         order.print_order()
+
+    red_apple = Apple("champion", "duże", 2.18)
+    old_potato = Potato("na frytki", "długi", 1.35)
+
+    apple_kg_amount = 2.5
+    potato_kg_amount = 3.74
+
+    print()
+    red_apple.print()
+    old_potato.print()
+    print(f"Cena {apple_kg_amount} kg jabłek wynosi: {red_apple.calculate_price(apple_kg_amount):.2f} zł")
+    print(f"Cena {potato_kg_amount} kg jabłek wynosi: {red_apple.calculate_price(potato_kg_amount):.2f} zł")
--- code/obiekty/store/Potato.py
+++ code/obiekty/store/Potato.py
@@ -3,3 +3,9 @@ class Potato:
         self.species_name = species_name
         self.size = size
         self.kg_price = kg_price
+
+    def calculate_price(self, kg_amount):
+        return self.kg_price * kg_amount
+
+    def print(self):
+        print(f"Gatunek: {self.species_name} | Rozmiar: {self.size} | Cena za kg: {self.kg_price}")
